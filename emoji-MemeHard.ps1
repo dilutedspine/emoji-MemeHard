6485849b87ecac20bf96dc6a35b0e60a46719906
Wesley Knight <31743477+dilutedspine@users.noreply.github.com>
2019-07-26 07:02:36 -0500

    Update emoji-MemeHard.ps1

--- emoji-MemeHard.ps1
+++ emoji-MemeHard.ps1
@@ -1,5 +1,6 @@
 #This script will insert a : in front of and behind every character that's not a space enabling you to meme your friends by spelling full messages with emojiis
 
+clear-host
 $phrase = Read-Host "Insert memehard string"
 $output = $null
 foreach($character in $phrase.ToCharArray()) {
